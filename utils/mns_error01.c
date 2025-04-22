b1c2423f846e333d655c224fe9ab4096f98afd27
Alban Gamay <agamay@1D1.42angouleme.fr>
2025-04-22 10:01:15 +0200

    aga_recurs merged. history cleared in errors

--- utils/mns_error01.c
+++ utils/mns_error01.c
@@ -20,6 +20,7 @@ void	redir_error(t_data *data, t_cmd *cmd, t_token *rdir_list)
 	cmd->rdir_list = NULL;
 	if (cmd->pid == 1)
 	{
+		rl_clear_history();
 		closer(2, cmd->pfd[0], cmd->pfd[1]);
 		ft_end(data);
 		exit(1);
@@ -36,6 +37,7 @@ void	isdir_error(t_data *data, t_cmd *cmd)
 	cmd->rdir_list = NULL;
 	if (cmd->pid == 1)
 	{
+		rl_clear_history();
 		closer(2, cmd->pfd[0], cmd->pfd[1]);
 		ft_end(data);
 		exit(126);
@@ -60,6 +62,7 @@ void	fct_error(t_data *data, t_cmd *cmd)
 	cmd->rdir_list = NULL;
 	if (cmd->pid == 1)
 	{
+		rl_clear_history();
 		closer(2, cmd->pfd[0], cmd->pfd[1]);
 		ft_end(data);
 		exit(127);
